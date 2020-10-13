5eae47aa4e9f5e0250d3712c06788d7428ebc555
Pedro Silva <pedrosilvavpsn@gmail.com>
2020-10-13 15:24:53 +0100

    first commit

--- raven/ufo/urls.py
+++ raven/ufo/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("all_events", views.all_events, name="all_events"),
+    path("all_locations", views.all_locations, name="all_locations"),
+    path("search_location", views.search_location, name="search_location"),
+    path("search_date", views.search_date, name="search_date"),
     path("<int:description_id>/", views.detail, name="detail"),
     path("<int:description_id>/location/", views.event, name="event"),
     path("<int:description_id>/date/", views.date, name="date"),
--- raven/ufo/views.py
+++ raven/ufo/views.py
@@ -4,16 +4,49 @@ from django.http import HttpResponse
 # from .models import Question
 from .models import Event
 
+
 # Create your views here.
 # def index(request):
 #     return HttpResponse("Hello World")
 
 def index(request):
-    context = {"event": Event.objects.all()}
-    return render(request, "ufo/index.html", context) 
+    # context = {"event": Event.objects.all()}
+    return render(request, "ufo/index.html") 
     # events = Event.objects.all()
     # output = "<br/>".join([f"<h2>Event</h2><p>{q.description}</p>" for q in events])
     # return HttpResponse(f"<h1>All Events Page</h1>{output}")
+
+def all_events(request):
+    context = {"event": Event.objects.all()}
+    return render(request, "ufo/all_events.html", context)
+
+def all_locations(request):
+    context = {"location": Event.objects.all()}
+    return render(request, "ufo/all_locations.html", context)
+
+def search_location(request):
+    qs = Event.objects.all()
+    search_location = request.GET.get('search_location')
+    if search_location != '' and search_location is not None:
+        qs = qs.filter(area__icontains=search_location)
+
+    context = {
+        'loc': qs
+    }
+
+    return render(request, "ufo/search_location.html", context)
+
+def search_date(request):
+    qs = Event.objects.all()
+    search_date = request.GET.get('search_date')
+    if search_date != '' and search_date is not None:
+        qs = qs.filter(date__icontains=search_date)
+
+    context = {
+        'date': qs
+    }
+
+    return render(request, "ufo/search_date.html", context)
 
 def detail(request, description_id):
     q = get_object_or_404(Event, pk=description_id)
